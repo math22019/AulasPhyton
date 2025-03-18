ef706dd0581dbac014f7991c865be90b2d489797
math22019 <matheusporto1912@gmai.com>
2025-03-17 22:48:34 -0300

    Primeira aula

--- PP.py
+++ PP.py
@@ -1,1 +1,21 @@
-print('Hello World')
+#Exercicío 1
+print('Matheus Porto\n')
+
+#Exercicío 2
+a = 3
+b = 5
+print(2*a*3*b)
+print()
+
+#Exercicío 3
+d = 2
+e = 3
+f = 4
+soma = d+e+f
+print(soma)
+
+z = float(input('numero 1 = '))
+x = float(input('numero 2 = '))
+c = float(input('numero 3 = '))
+soma = z+x+c
+print(f'soma = {soma: .2f}')
