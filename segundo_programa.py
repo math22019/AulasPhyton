9bc4694c2e95f8db6a9f16897fd5b99cd06b7ef3
math22019 <matheusporto1912@gmail.com>
2025-03-24 22:52:39 -0300

    Primeira aula

---
+++ segundo_programa.py
@@ -0,0 +1,18 @@
+d = True
+print(type(d))
+
+nome = "Matheus Martins Porto"
+print(type(nome))
+
+print(10**3)
+print(10//3)
+print(10%3)
+
+# nota = 8
+# media = 7
+# aprovado = nota >= media
+# print(aprovado)
+
+nome_completo = input("Nome completo: ")
+idade = int(input("Idade: "))
+altura = float(input("Altura: "))
