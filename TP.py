44c4f200e76459c190a754b8e347e16c4bd5d0e5
math22019 <matheusporto1912@gmail.com>
2025-03-31 22:52:04 -0300

    Primeira aula

---
+++ TP.py
@@ -0,0 +1,53 @@
+#Exercío zero
+
+# media_final = float(input("Media final: "))
+#
+# if media_final >= 6:
+#     print("Aprovado")
+# else:
+# #     print("Reprovado")
+#
+# a = (input("Primeiro valor: "))
+# b = (input("Segundo valor: "))
+# if a > b:
+#     print("O primeiro valor é maior")
+# if b > a:
+#     print("O segundo valor é maior")
+# if a == b:
+#     print("Os valores são iguais")
+
+#Primeiro exercício
+
+# nome = (input("\nNome do condutor: "))
+#
+# velocidade_do_veiculo = float(input("\nVelocidade do veículo: "))
+#
+# velocidade_limite = 80
+#
+# multa = (velocidade_do_veiculo - velocidade_limite)*50
+#
+# if velocidade_do_veiculo > velocidade_limite :
+#     print(f"\nMultado em = R$ {multa}")
+#
+# else:
+#     print("\nTudo correto :3")
+
+#Segundo exercício
+
+# num1 = float(input("\nPrimeiro número: "))
+# num2 = float(input("\nSegundo número: "))
+# num3 = float(input("\nTerceiro número: "))
+#
+#
+# maximo = max (num1,num2,num3)
+#
+# minimo = min (num1,num2,num3)
+#
+# print("\nO numero maior é",{maximo})
+# print("\nO numero menor é",{minimo})
+
+#Terceiro exercício
+
+salario = float(input("Salário: "))
+
+if salario > 1250
